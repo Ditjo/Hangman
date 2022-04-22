5d3a15d507847aa615d2aa63deb3177498eb7d5c
Ditjo <mads933c@elev.tec.dk>
2022-04-22 13:58:52 +0200

    Add project files.

---
+++ Hangman/Program.cs
@@ -0,0 +1,82 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Hangman
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word, guess, AllLetters = "";
+            //char letter;
+            //char letter;
+
+            Console.WriteLine("Welcome to Hangman");
+            Console.WriteLine("");
+
+            word = Tools.Word();
+            word = word.ToUpper();
+
+            List<string> guessedWords = new List<string>();
+            for (int i = 0; i < word.Length; i++)
+            {
+                string filler = "_ ";
+                guessedWords.Add(filler);
+            }
+
+            bool win = false, lose = false;
+            int count = 0;
+
+            do
+            {
+                Console.WriteLine("");
+                Console.Write("Indtast bogstav: ");
+
+                ConsoleKeyInfo letter;
+                letter = Console.ReadKey(true);
+                guess = Convert.ToString(letter.Key);
+                //guess = Console.ReadLine();
+                guess = guess.ToUpper();
+                Console.WriteLine("");
+                AllLetters = AllLetters + guess + " ";
+                Console.WriteLine(AllLetters);
+
+                //bool win = false,lose = false;
+
+                if (word.Contains(guess))
+                {
+                    win = Tools.Correct(word, guess, guessedWords);
+                }
+                else
+                {
+                    count++;
+                    lose = Tools.Mistake(count);
+                }
+
+                foreach (var item in guessedWords)
+                {
+                    Console.Write(item);
+                }
+                
+
+            } while (win == false && lose == false);
+
+            if (win == true)
+            {
+                Console.WriteLine("Winner");
+            }
+            else if(lose == true)
+            {
+                Console.WriteLine("Loser");
+            }
+            else
+            {
+                Console.WriteLine("Game Broke");
+            }
+            
+            //ConsoleKeyInfo letter;
+            //letter = Console.ReadKey(true);
+            //if(word.Contains(letter.Key))
+            //if (Cho.Key == ConsoleKey.P);
+        }
+    }
+}
---
+++ Hangman/Tools.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    internal class Tools
+    {
+        static internal string Word()
+        {
+            //string as a return
+            string word;
+
+            //A list is created
+            //The items from a string-array is inserted into the list.
+            List<string> words = new List<string>();
+            string[] wordsArray = { "Cat", "Dog", "Bird", "Hamster", "Horse" };
+            words.AddRange(wordsArray);
+
+            //A randomiser is created. It is as large as the number of items in the list.
+            Random num = new Random();
+            word = words[num.Next(words.Count)];
+            //It picks a random item(word) from the list and return it.
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                Console.Write("_ ");
+            }
+            return word;
+        }
+        
+        static internal bool Correct(string word, string guess, List<string> guessedWords)
+        {
+
+            char[] charWord = word.ToCharArray();
+            char charGuess = Convert.ToChar(guess);
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (charWord[i] == charGuess)
+                {
+                    guessedWords.RemoveAt(i);
+                    guessedWords.Insert(i, guess + " ");
+                }
+            }
+            if (guessedWords.Contains("_ "))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+            
+            //foreach (var item in guessedWords)
+            //{
+            //    Console.Write(item);
+            //}
+
+        }
+        static internal bool Mistake(int count)
+        {
+            
+            
+
+            Console.WriteLine(count);
+
+            if(count == 1)
+            {
+                Console.WriteLine("X");
+                return false;
+            }
+            else if (count == 2)
+            {
+                Console.WriteLine("XX");
+                return false;
+            }
+            else if (count == 3)
+            {
+                Console.WriteLine("XXX");
+                return false;
+            }
+            else if (count == 4)
+            {
+                Console.WriteLine("XXXX");
+                return false;
+            }
+            else if (count == 5)
+            {
+                Console.WriteLine("XXXXX");
+                return false;
+            }
+            else if (count == 6)
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Game Broke");
+                return true;
+            }
+            
+        }
+    }
+}
