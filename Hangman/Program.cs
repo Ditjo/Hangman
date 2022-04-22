c285c5150c2d78f2063fa027e021cba20f403250
Ditjo <mads933c@elev.tec.dk>
2022-04-22 16:17:53 +0200

    Finish game.

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -4,18 +4,26 @@ namespace Hangman
 {
     internal class Program
     {
+        static int left = 3;
+
         static void Main(string[] args)
         {
+            //Instantiating different objects 
             string word, guess, AllLetters = "";
-            //char letter;
-            //char letter;
+            bool win = false, lose = false;
+            int count = 0;
 
+            //Title of game 
+            Console.SetCursorPosition(left, 2);
             Console.WriteLine("Welcome to Hangman");
-            Console.WriteLine("");
 
+            //Calls the main background graphic
+            Tools.graphics();
+
+            //calls for a random word
             word = Tools.Word();
-            word = word.ToUpper();
-
+            
+            //Creats a list and fills it with _ maching the words length
             List<string> guessedWords = new List<string>();
             for (int i = 0; i < word.Length; i++)
             {
@@ -23,24 +31,26 @@ namespace Hangman
                 guessedWords.Add(filler);
             }
 
-            bool win = false, lose = false;
-            int count = 0;
-
             do
             {
-                Console.WriteLine("");
+                //input graphic
+                Console.SetCursorPosition(left, 11);
                 Console.Write("Indtast bogstav: ");
 
-                ConsoleKeyInfo letter;
-                letter = Console.ReadKey(true);
-                guess = Convert.ToString(letter.Key);
-                //guess = Console.ReadLine();
-                guess = guess.ToUpper();
-                Console.WriteLine("");
-                AllLetters = AllLetters + guess + " ";
-                Console.WriteLine(AllLetters);
+                do
+                {
+                    //you press a key and it makes it your guess
+                    ConsoleKeyInfo letter;
+                    letter = Console.ReadKey(true);
+                    guess = Convert.ToString(letter.Key);
+                    guess = guess.ToUpper();
 
-                //bool win = false,lose = false;
+                }while(guess.Length != 1);
+
+                AllLetters = AllLetters + guess + " ";
+
+                Console.SetCursorPosition(left, 9);
+                Console.WriteLine(AllLetters);
 
                 if (word.Contains(guess))
                 {
@@ -52,31 +62,31 @@ namespace Hangman
                     lose = Tools.Mistake(count);
                 }
 
+                Console.SetCursorPosition(left, 5);
                 foreach (var item in guessedWords)
                 {
                     Console.Write(item);
                 }
-                
+
 
             } while (win == false && lose == false);
 
+            Console.SetCursorPosition(left, 13);
             if (win == true)
             {
-                Console.WriteLine("Winner");
+                Console.WriteLine("You Win");
             }
-            else if(lose == true)
+            else if (lose == true)
             {
-                Console.WriteLine("Loser");
+                Console.WriteLine("You Lose");
+                Console.SetCursorPosition(left, 15);
+                Console.WriteLine($"The word was {word}");
             }
             else
             {
                 Console.WriteLine("Game Broke");
             }
-            
-            //ConsoleKeyInfo letter;
-            //letter = Console.ReadKey(true);
-            //if(word.Contains(letter.Key))
-            //if (Cho.Key == ConsoleKey.P);
+
         }
     }
 }
--- Hangman/Tools.cs
+++ Hangman/Tools.cs
@@ -8,6 +8,7 @@ namespace Hangman
 {
     internal class Tools
     {
+        static int left = 3;
         static internal string Word()
         {
             //string as a return
@@ -16,14 +17,19 @@ namespace Hangman
             //A list is created
             //The items from a string-array is inserted into the list.
             List<string> words = new List<string>();
-            string[] wordsArray = { "Cat", "Dog", "Bird", "Hamster", "Horse" };
+            string[] wordsArray = { "Cat","Mars","Sheriff","Hamster","Horse","Titanic",
+            "Japan","SpaceX","Zebra","Pizza","York","Carthage","Cree","Whale","Wellington",
+            "Painting","Nuuk","Cow","Squid","Attenborough","Pen","Cola","Paper","Locomotive"};
             words.AddRange(wordsArray);
 
             //A randomiser is created. It is as large as the number of items in the list.
             Random num = new Random();
             word = words[num.Next(words.Count)];
+            word = word.ToUpper();
             //It picks a random item(word) from the list and return it.
 
+            //The words length is spelled out using _
+            Console.SetCursorPosition(left, 5);
             for (int i = 0; i < word.Length; i++)
             {
                 Console.Write("_ ");
@@ -31,12 +37,32 @@ namespace Hangman
             return word;
         }
         
+        static internal void graphics()
+        {
+            //Graphic method. 
+            int gpu = 35;
+            Console.SetCursorPosition(gpu, 2);
+            Console.WriteLine(" _________\n");
+            Console.SetCursorPosition(gpu, 3);
+            Console.WriteLine(" |/      |\n");
+            Console.SetCursorPosition(gpu, 4);
+            Console.WriteLine(" |\n");
+            Console.SetCursorPosition(gpu, 5);
+            Console.WriteLine(" |\n");
+            Console.SetCursorPosition(gpu, 6);
+            Console.WriteLine(" |\n");
+            Console.SetCursorPosition(gpu, 7);
+            Console.WriteLine("_|_");
+        }
         static internal bool Correct(string word, string guess, List<string> guessedWords)
         {
-
+        //If a letter is correct
+            
+            //The Word and guess is convertet into char
             char[] charWord = word.ToCharArray();
             char charGuess = Convert.ToChar(guess);
 
+            //looks for an replaces the letter that was correct
             for (int i = 0; i < word.Length; i++)
             {
                 if (charWord[i] == charGuess)
@@ -45,6 +71,7 @@ namespace Hangman
                     guessedWords.Insert(i, guess + " ");
                 }
             }
+            //If there are no more _ the word is done and you win (sets to true).
             if (guessedWords.Contains("_ "))
             {
                 return false;
@@ -54,50 +81,54 @@ namespace Hangman
                 return true;
             }
             
-            //foreach (var item in guessedWords)
-            //{
-            //    Console.Write(item);
-            //}
-
         }
         static internal bool Mistake(int count)
         {
-            
-            
+        //if the letter is wrong 
+            //gpu is for use in graphics
+            int gpu = 44;
 
-            Console.WriteLine(count);
-
+            //it counts and draws the graphics on by one.
             if(count == 1)
             {
-                Console.WriteLine("X");
+                Console.SetCursorPosition(gpu, 4);
+                Console.WriteLine("o");
                 return false;
             }
             else if (count == 2)
             {
-                Console.WriteLine("XX");
+                Console.SetCursorPosition(gpu, 5);
+                Console.WriteLine("|");
                 return false;
             }
             else if (count == 3)
             {
-                Console.WriteLine("XXX");
+                Console.SetCursorPosition(gpu-1, 5);
+                Console.WriteLine("/");
                 return false;
             }
             else if (count == 4)
             {
-                Console.WriteLine("XXXX");
+                Console.SetCursorPosition(gpu+1, 5);
+                Console.WriteLine("\\");
                 return false;
             }
             else if (count == 5)
             {
-                Console.WriteLine("XXXXX");
+                Console.SetCursorPosition(gpu-1, 6);
+                Console.WriteLine("/");
                 return false;
             }
             else if (count == 6)
             {
+                //if it counts to 6 you are dead.
+                Console.SetCursorPosition(gpu+1, 6);
+                Console.WriteLine("\\");
                 return true;
             }
             else
             {
+                //incase something happens here.
                 Console.WriteLine("Game Broke");
                 return true;
             }
